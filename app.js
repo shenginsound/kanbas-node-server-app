c5603ce69fdf415fabcd69582f8971521ab24889
鄭家昇 <zhengjiasheng@zhengjiashengdeMacBook-Pro.local>
2023-11-19 15:00:31 -0500

    third commit

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // const express = require('express')
 import express from 'express';
 import Hello from './hello.js';
-import Lab5 from "./lab5.js";
+import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from "./Modules/routes.js";
 import "dotenv/config";
-const app = express()
-app.use(express.json());
+const app = express();
 app.use(cors());
+app.use(express.json());
+
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
