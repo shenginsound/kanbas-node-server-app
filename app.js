87e74c0930f2d06d1f12b1a79a8b8e5675df16e4
鄭家昇 <zhengjiasheng@zhengjiashengdeMacBook-Pro.local>
2023-12-01 15:48:45 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -6,9 +6,27 @@ import cors from "cors";
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import session from "express-session";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 const app = express();
-app.use(cors());
+app.use(cors(
+    {credentials: true,
+   origin: "http://localhost:3000",}
+));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  app.use(
+    session(sessionOptions)
+  );
+  
 app.use(express.json());
+UserRoutes(app);
 
 ModuleRoutes(app);
 CourseRoutes(app);
